b0a0988aac29724feb523da47b2f6927bd59fd97
lennan747 <lennan747@gmail.com>
2019-10-21 18:43:22 +0800

    安装跨域请求库 cors配置 fractal库安装 fractal是一个数据转化层

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // 跨域请求
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,18 +16,19 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api\V1'
+    'namespace' => 'App\Http\Controllers\Api\V1',
+    'middleware' => ['serializer:array','cors']
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store');
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store');
         // 用户注册
         $api->post('users', 'UsersController@store');
-        // 图片验证码
-        $api->post('captchas', 'CaptchasController@store');
     });
 });
