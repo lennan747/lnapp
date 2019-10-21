e704b1f69b540a8c6058b0be2a22c14226bf1d22
lennan747 <lennan747@gmail.com>
2019-10-21 14:02:13 +0800

    节流处理，防止攻击

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,14 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api\V1'
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store');
-    // 用户注册
-    $api->post('users', 'UsersController@store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store');
+        // 用户注册
+        $api->post('users', 'UsersController@store');
+    });
 });
